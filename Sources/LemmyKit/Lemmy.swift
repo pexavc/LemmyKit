60bef548d63c4a98244ccd12ac8a6c0d980f140f
PEXAVC <89366826+pexavc@users.noreply.github.com>
2023-07-25 17:02:05 -0700

    search functionality

--- Sources/LemmyKit/Lemmy.swift
+++ Sources/LemmyKit/Lemmy.swift
@@ -374,7 +374,7 @@ public extension Lemmy {
                                               page: page,
                                               limit: limit,
                                               sort: sort,
-                                              auth: auth,
+                                              auth: auth ?? shared.auth,
                                               isLocal: false)
         //Fetch local community
         } else {
@@ -629,5 +629,62 @@ public extension Lemmy {
                                           post: post,
                                           parent: parent,
                                           auth: validAuth)
+    }
+}
+
+//MARK: -- Search
+
+public extension Lemmy {
+    @discardableResult
+    func search(_ q: String,
+                type_: SearchType,
+                communityId: CommunityId? = nil,
+                communityName: String? = nil,
+                creatorId: PersonId? = nil,
+                sort: SortType = .hot,
+                listingType: ListingType = .all,
+                page: Int?,
+                limit: Int?,
+                auth: String?) async -> Search.Response? {
+        guard let result = try? await api.request(
+            Search(q: q,
+                   community_id: communityId,
+                   community_name: communityName,
+                   creator_id: creatorId,
+                   type_: type_,
+                   sort: sort,
+                   listing_type: listingType,
+                   page: page,
+                   limit: limit,
+                   auth: auth ?? self.auth)
+        ).async() else {
+            return nil
+        }
+        
+        return result
+    }
+    @discardableResult
+    static func search(_ q: String,
+                       type_: SearchType,
+                       communityId: CommunityId? = nil,
+                       communityName: String? = nil,
+                       creatorId: PersonId? = nil,
+                       sort: SortType = .hot,
+                       listingType: ListingType = .all,
+                       page: Int?,
+                       limit: Int?,
+                       auth: String? = nil) async -> Search.Response? {
+        guard let shared else { return nil }
+        
+        return await shared.search(q,
+                                   type_: type_,
+                                   communityId: communityId,
+                                   communityName: communityName,
+                                   creatorId: creatorId,
+                                   sort: sort,
+                                   listingType: listingType,
+                                   page: page,
+                                   limit: limit,
+                                   auth: auth)
     }
 }
