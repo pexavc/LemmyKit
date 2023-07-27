600d54f8684506e91ecf4d454a9ef1d5e8b5c350
PEXAVC <89366826+pexavc@users.noreply.github.com>
2023-07-26 17:01:19 -0700

    saveusersettings + persondetails

--- Sources/LemmyKit/Lemmy.swift
+++ Sources/LemmyKit/Lemmy.swift
@@ -697,3 +697,165 @@ public extension Lemmy {
                                    auth: auth)
     }
 }
+
+//MARK: -- User
+
+public extension Lemmy {
+    
+    func saveUserSettings(show_nsfw: Bool? = nil,
+                          show_scores: Bool? = nil,
+                          theme: String? = nil,
+                          default_sort_type: SortType? = nil,
+                          default_listing_type: ListingType? = nil,
+                          interface_language: String? = nil,
+                          avatar: String? = nil,
+                          banner: String? = nil,
+                          display_name: String? = nil,
+                          email: String? = nil,
+                          bio: String? = nil,
+                          matrix_user_id: String? = nil,
+                          show_avatars: Bool? = nil,
+                          send_notifications_to_email: Bool? = nil,
+                          bot_account: Bool? = nil,
+                          show_bot_accounts: Bool? = nil,
+                          show_read_posts: Bool? = nil,
+                          show_new_post_notifs: Bool? = nil,
+                          discussion_languages: [LanguageId]? = nil,
+                          generate_totp_2fa: Bool? = nil,
+                          auth: String,
+                          open_links_in_new_tab: Bool? = nil
+                      ) async -> LoginResponse? {
+        guard let result = try? await api.request(
+            SaveUserSettings(show_nsfw: show_nsfw,
+                             show_scores: show_scores,
+                             theme: theme,
+                             default_sort_type: default_sort_type,
+                             default_listing_type: default_listing_type,
+                             interface_language: interface_language,
+                             avatar: avatar,
+                             banner: banner,
+                             display_name: display_name,
+                             email: email,
+                             bio: bio,
+                             matrix_user_id: matrix_user_id,
+                             show_avatars: show_avatars,
+                             send_notifications_to_email: send_notifications_to_email,
+                             bot_account: bot_account,
+                             show_bot_accounts: show_bot_accounts,
+                             show_read_posts: show_read_posts,
+                             show_new_post_notifs: show_new_post_notifs,
+                             discussion_languages: discussion_languages,
+                             generate_totp_2fa: generate_totp_2fa,
+                             auth: auth,
+                             open_links_in_new_tab: open_links_in_new_tab)
+        ).async() else {
+            return nil
+        }
+        
+        return result
+    }
+    static func saveUserSettings(show_nsfw: Bool? = nil,
+                                 show_scores: Bool? = nil,
+                                 theme: String? = nil,
+                                 default_sort_type: SortType? = nil,
+                                 default_listing_type: ListingType? = nil,
+                                 interface_language: String? = nil,
+                                 avatar: String? = nil,
+                                 banner: String? = nil,
+                                 display_name: String? = nil,
+                                 email: String? = nil,
+                                 bio: String? = nil,
+                                 matrix_user_id: String? = nil,
+                                 show_avatars: Bool? = nil,
+                                 send_notifications_to_email: Bool? = nil,
+                                 bot_account: Bool? = nil,
+                                 show_bot_accounts: Bool? = nil,
+                                 show_read_posts: Bool? = nil,
+                                 show_new_post_notifs: Bool? = nil,
+                                 discussion_languages: [LanguageId]? = nil,
+                                 generate_totp_2fa: Bool? = nil,
+                                 auth: String,
+                                 open_links_in_new_tab: Bool? = nil
+                             ) async -> LoginResponse? {
+        guard let shared else { return nil }
+        
+        return await shared.saveUserSettings(show_nsfw: show_nsfw,
+                                             show_scores: show_scores,
+                                             theme: theme,
+                                             default_sort_type: default_sort_type,
+                                             default_listing_type: default_listing_type,
+                                             interface_language: interface_language,
+                                             avatar: avatar,
+                                             banner: banner,
+                                             display_name: display_name,
+                                             email: email,
+                                             bio: bio,
+                                             matrix_user_id: matrix_user_id,
+                                             show_avatars: show_avatars,
+                                             send_notifications_to_email: send_notifications_to_email,
+                                             bot_account: bot_account,
+                                             show_bot_accounts: show_bot_accounts,
+                                             show_read_posts: show_read_posts,
+                                             show_new_post_notifs: show_new_post_notifs,
+                                             discussion_languages: discussion_languages,
+                                             generate_totp_2fa: generate_totp_2fa,
+                                             auth: auth,
+                                             open_links_in_new_tab: open_links_in_new_tab)
+    }
+    
+    func details(_ person_id: PersonId? = nil,
+              sort: SortType? = nil,
+              page: Int? = nil,
+              limit: Int? = nil,
+              community_id: CommunityId? = nil,
+              saved_only: Bool? = nil,
+              auth: String? = nil) async -> GetPersonDetailsResponse? {
+        guard let result = try? await api.request(
+            GetPersonDetails(person_id: person_id,
+                             sort: sort,
+                             page: page,
+                             limit: limit,
+                             community_id: community_id,
+                             saved_only: saved_only,
+                             auth: auth ?? self.auth)
+        ).async() else {
+            return nil
+        }
+        
+        return result
+    }
+    static func details(_ person: Person? = nil,
+                        sort: SortType? = nil,
+                        page: Int? = nil,
+                        limit: Int? = nil,
+                        community: Community? = nil,
+                        saved_only: Bool? = nil,
+                        auth: String? = nil,
+                        useBase: Bool = true) async -> GetPersonDetailsResponse? {
+        guard let shared else { return nil }
+        
+        if useBase == false,
+           let community,
+           let domain = LemmyKit.sanitize(community.actor_id).host { //getInstancedDomain(community: community) {
+            let instancedLemmy: Lemmy = .init(apiUrl: domain)
+            
+            return await instancedLemmy.details(person?.id,
+                                        sort: sort,
+                                        page: page,
+                                        limit: limit,
+                                        community_id: community.id,
+                                        saved_only: saved_only,
+                                        auth: auth)
+        //Fetch local community
+        } else {
+            
+            return await shared.details(person?.id,
+                                        sort: sort,
+                                        page: page,
+                                        limit: limit,
+                                        community_id: community?.id,
+                                        saved_only: saved_only,
+                                        auth: auth)
+        }
+    }
+}
