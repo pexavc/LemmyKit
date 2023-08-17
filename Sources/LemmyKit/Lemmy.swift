183acb85a519cf46980c385f0ddfabf3f7c3ef50
PEXAVC <89366826+pexavc@users.noreply.github.com>
2023-08-17 01:41:20 -0700

    editPost + editComment

--- Sources/LemmyKit/Lemmy.swift
+++ Sources/LemmyKit/Lemmy.swift
@@ -926,6 +926,56 @@ public extension Lemmy {
     }
     
     @discardableResult
+    func editPost(_ postId: PostId,
+                    title: String,
+                    url: String? = nil,
+                    body: String? = nil,
+                    nsfw: Bool = false,
+                    language_id: LanguageId? = nil,
+                    auth: String) async -> PostView? {
+        guard let result = try? await api.request(
+            EditPost(
+                post_id: postId,
+                name: title,
+                url: url,
+                body: body,
+                nsfw: nsfw,
+                language_id: language_id,
+                auth: auth
+            )
+        ).async() else {
+            return nil
+        }
+        
+        return result.post_view
+    }
+    @discardableResult
+    static func editPost(_ postId: PostId,
+                         title: String,
+                         url: String? = nil,
+                         body: String? = nil,
+                         nsfw: Bool = false,
+                         language_id: LanguageId? = nil,
+                         auth: String? = nil) async -> PostView? {
+        guard let shared else { return nil }
+        
+        let validAuth: String? = auth ?? shared.auth
+        
+        guard let validAuth else {
+            LemmyLog("Authentication required")
+            return nil
+        }
+        
+        return await shared.editPost(postId,
+                                     title: title,
+                                     url: url,
+                                     body: body,
+                                     nsfw: nsfw,
+                                     language_id: language_id,
+                                     auth: validAuth)
+    }
+    
+    @discardableResult
     func createComment(_ content: String,
                        post: Post,
                        parent: Comment? = nil,
@@ -959,6 +1009,47 @@ public extension Lemmy {
                                           post: post,
                                           parent: parent,
                                           auth: validAuth)
+    }
+    
+    @discardableResult
+    func editComment(_ comment_id: CommentId,
+                     content: String? = nil,
+                     language_id: LanguageId? = nil,
+                     form_id: String? = nil,
+                     auth: String) async -> Comment? {
+        guard let result = try? await api.request(
+            EditComment(comment_id: comment_id,
+                        content: content,
+                        language_id: language_id,
+                        form_id: form_id,
+                        auth: auth
+            )
+        ).async() else {
+            return nil
+        }
+        
+        return result.comment_view.comment
+    }
+    @discardableResult
+    static func editComment(_ comment_id: CommentId,
+                            content: String? = nil,
+                            language_id: LanguageId? = nil,
+                            form_id: String? = nil,
+                            auth: String? = nil) async -> Comment? {
+        guard let shared else { return nil }
+        
+        let validAuth: String? = auth ?? shared.auth
+        
+        guard let validAuth else {
+            LemmyLog("Authentication required")
+            return nil
+        }
+        
+        return await shared.editComment(comment_id,
+                                        content: content,
+                                        language_id: language_id,
+                                        form_id: form_id,
+                                        auth: validAuth)
     }
 }
 
